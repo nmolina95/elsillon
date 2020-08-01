3c17b9cddc4423722b183b3b9d2ed47be5ec09a3
nmolina95 <ignaciomolina.95@hotmail.com>
2020-08-01 04:31:10 -0300

    Creada ruta de index para el proyecto.

--- web/app.js
+++ web/app.js
@@ -2,3 +2,15 @@ const express = require('express');
 const app = express();
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
+
+//Setting template engine
+app.set('view engine', 'ejs');
+
+// Setear la carpeta public para archivos estaticos
+app.use(express.static(__dirname + '/public'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(session({ secret: 'piratas en el mapa' }));
+app.use(cookieParser());
+
+app.listen('3000', () => console.log('Servidor ejecutando en puerto 3000'));
